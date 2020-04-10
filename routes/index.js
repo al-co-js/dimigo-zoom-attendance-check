78b3e03270df422f53a099058ce652f42be08f1d
심재성 <shimjs8@naver.com>
2020-04-11 02:08:41 +0900

    feat: test code

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', async () => {
-  
+router.post('/', async (req, res) => {
+  const { event, payload } = req;
+  if (!(event && payload)) {
+    res.sendStatus(412);
+    return;
+  }
+
+  console.log(event, payload);
+  res.sendStatus(200);
 });
 
 export default router;
