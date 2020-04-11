2518638b5988eba2f1d25a7ad0465fed053362e5
심재성 <shimjs8@naver.com>
2020-04-11 15:47:42 +0900

    refactor: attach files

--- src/routes/controller.js
+++
@@ -1,8 +0,0 @@
-function participantJoined(userName, joinTime) {}
-
-export default function webhookReceived(data) {
-  if (data.event === 'meeting.participant_joined') {
-    const { participant } = data.payload.object;
-    participantJoined(participant.user_name, participant.join_time);
-  }
-}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,26 @@
 import { Router } from 'express';
-import webhookReceived from './controller';
 
 const router = Router();
 
+const participantJoined = (userName, joinTime) => {};
+
+const webhookReceived = (data) => {
+  if (data.event === 'meeting.participant_joined') {
+    const { participant } = data.payload.object;
+    return participantJoined(participant.user_name, participant.join_time);
+  }
+  return false;
+};
+
 router.post('/', (req, res) => {
   const data = req.body;
-  webhookReceived(data);
+
+  const result = webhookReceived(data);
+  if (!result) {
+    res.sendStatus(500);
+    return;
+  }
+
   res.sendStatus(200);
 });
 
