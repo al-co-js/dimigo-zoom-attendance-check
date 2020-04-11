a4b5b549c4b9be3ba74d08f644bfe00b62b9244f
SeungHyunMin <contact@seunghyun.in>
2020-04-11 17:07:29 +0900

    refactor : sheet api

--- src/api/sheet-api.js
+++ src/api/sheet-api.js
@@ -1,35 +1,35 @@
 import fs from 'fs';
 import { google } from 'googleapis';
 
-const authorize = (credentials, callback) => {
-  const {
-    client_secret: clientSecret,
-    client_id: clientId,
-    redirect_uris: redirectUris,
-  } = credentials.installed;
+const authorize = (callback) => {
+  fs.readFile('credentials.json', (_err, content) => {
+    const {
+      client_secret: clientSecret,
+      client_id: clientId,
+      redirect_uris: redirectUris,
+    } = JSON.parse(content).installed;
 
-  const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUris[0]);
+    const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUris[0]);
 
-  fs.readFile('token.json', (_err, token) => {
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
+    fs.readFile('token.json', (_err, token) => {
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+    });
   });
 };
 
 class Sheet {
   constructor() {
     this.getValues = (range, callback) => {
-      fs.readFile('credentials.json', (_err, content) => {
-        authorize(JSON.parse(content), (auth) => {
-          const sheets = google.sheets({ version: 'v4', auth });
-          sheets.spreadsheets.values.get(
-            {
-              spreadsheetId: process.env.SHEET_ID,
-              range,
-            },
-            callback,
-          );
-        });
+      authorize((auth) => {
+        const sheets = google.sheets({ version: 'v4', auth });
+        sheets.spreadsheets.values.get(
+          {
+            spreadsheetId: process.env.SHEET_ID,
+            range,
+          },
+          callback,
+        );
       });
     };
   }
