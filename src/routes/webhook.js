5c75d95f794e1844bb85e8960c3a1357b85ca8f9
심재성 <shimjs8@naver.com>
2020-04-14 05:41:52 +0900

    fix: test code

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -46,9 +46,9 @@ const getCurrentTime = (timeData, now, day) => {
     }
   });
   if (currentTime !== undefined) return currentTime;
-  console.log(typeof now);
+  console.log(now);
 
-  throw Error(`출석하는 시간이 아닙니다: ${parseInt(now / 60, 10)}시 ${now % 60}분`);
+  throw Error(`출석하는 시간이 아닙니다: ${now / 60}시 ${now % 60}분`);
 };
 
 const getParticipantStatus = (timeData, now, currentTime) => {
