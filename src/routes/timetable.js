cccdf4cf176b72f49ca48cb0c94f761221af0242
SeungHyunMin <contact@seunghyun.in>
2020-04-13 20:22:32 +0900

    feat : /timetable

--- src/routes/timetable.js
+++ src/routes/timetable.js
@@ -1,7 +1,21 @@
 import { Router } from 'express';
+import { promisify } from 'util';
+import fs from 'fs';
 
 const router = Router();
+const writeFileAsync = promisify(fs.writeFile);
 
-router.post('/', () => {});
+router.post('/', async (req, res) => {
+  const data = req.body;
+  try {
+    await writeFileAsync('timetable.json', JSON.stringify(data));
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+    res.sendStatus(500);
+    return;
+  }
+  res.sendStatus(200);
+});
 
 export default router;
